b66e8c2c3bf25ee2fdcd2da4aaf2fbf47c311e67
Kanishka <lkanishka200@gmail.com>
2022-08-15 06:41:10 +1000

    Add shop component

---
+++ ShopBook/Controllers/ShopController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopBook.Models;
+
+namespace ShopBook.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Random()
+        {
+            var shop = new Shop() { Name = "RACT" };
+            return View(shop);
+        }
+    }
+}
